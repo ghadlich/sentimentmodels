997ffd4ee6d0a7476acb7f8cf8d7dd5c873ec6fc
Grant Hadlich <ghadlich@gmail.com>
2021-05-07 21:08:18 -0700

    Adding in Path for submodule use

--- get_models.py
+++ get_models.py
@@ -20,6 +20,9 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE. 
+import sys
+sys.path.append("../sentimentmodels")
+
 from vader import VaderModel
 from naive_bayes import NaiveBayesModel
 from transformer import TransformerModel
